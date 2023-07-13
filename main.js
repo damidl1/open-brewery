4dd85c38543131fad4fb24998f3399a585163993
Damiano <d.dilionardo@gmail.com>
2023-07-13 18:01:17 +0200

    primo aggiornamento task

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-let pageNumber = 1;
+// let pageNumber = 1;
 
-DBService.getBreweries(pageNumber)
-.then(breweries => render(breweries));  // per chiamare le birrerie 
+
+
+// DBService.getBreweries(pageNumber)
+// .then(breweries => render(breweries));  // per chiamare le birrerie 
+
+let pageNumber = 1;
+let breweries;
+
+breweries = DBService.getBreweries()
+.then(breweries => render(breweries));  
+
 
 
 
@@ -15,6 +24,66 @@ function render(breweries) {
 
 }
 
+
+const mainContainer = document.getElementById('main-container');
+mainContainer.innerHTML = '';
+
+for (let i = 0; i < breweries.length; i++) {
+    
+    const brewery = breweries[i];
+
+    const breweryCard = document.createElement('div');
+
+    breweryCard.classList.add('brewery-card');
+
+    const nameCard = document.createElement('span');
+    const typeCard = document.createElement('span');
+    const addressCard = document.createElement('span');
+    const streetCard = document.createElement('span');
+    const stateCard = document.createElement('span');
+    const cityCard = document.createElement('span');
+    const countryCard = document.createElement('span');
+    const stateProvinceCard = document.createElement('span');
+    const phoneCard = document.createElement('span');
+    const websiteCard = document.createElement('span');
+    
+    const nameNode = document.createTextNode('Name: ' + brewery.name + '\n');
+    const typeNode = document.createTextNode('Brewery Type: ' + brewery_type + '\n');
+    const addressNode = document.createTextNode('Address: ' + brewery.address_1 + '\n');
+    const streetNode = document.createTextNode('Street: ' + brewery.street + '\n');
+    const cityNode = document.createTextNode('City: ' + brewery.city + '\n');
+    const stateProvinceNode = document.createTextNode('State Province: ' + brewery.state_province + '\n');
+    const stateNode = document.createTextNode('State: ' + brewery.state + '\n');
+    const countryNode = document.createTextNode('Country: ' + brewery.country + '\n');
+    const phoneNode = document.createTextNode('Phone: ' + brewery.phone + '\n');
+    const websiteNode = document.createTextNode('Website: ' + brewery.website_url);
+
+    nameCard.appendChild(nameNode);
+    typeCard.appendChild(typeNode);
+    addressCard.appendChild(addressNode);
+    streetCard.appendChild(streetNode);
+    cityCard.appendChild(cityNode);
+    stateProvinceCard.appendChild(stateProvinceNode);
+    stateCard.appendChild(stateNode);
+    countryCard.appendChild(countryNode);
+    phoneCard.appendChild(phoneNode);
+    websiteCard.appendChild(websiteNode);
+
+    breweryCard.appendChild(nameCard);
+    breweryCard.appendChild(typeCard);
+    breweryCard.appendChild(addressCard);
+    breweryCard.appendChild(streetCard);
+    breweryCard.appendChild(cityCard);
+    breweryCard.appendChild(stateProvinceCard);
+    breweryCard.appendChild(stateCard);
+    breweryCard.appendChild(countryCard);
+    breweryCard.appendChild(phoneCard);
+    breweryCard.appendChild(websiteCard);
+
+    main-container.appendChild(breweryCard);
+}
+
+render();
 
 
 function previous() {
--- services/db-service.js
+++ services/db-service.js
@@ -5,7 +5,7 @@ class DBService{
 
         const url = 'https://api.openbrewerydb.org/v1/breweries?per_page=10&page=' + pageNumber;
          
-        console.log(url);
+        // console.log(url);
          
         return fetch(url)  
         .then(resp => resp.json())  // possiamo costruire una promise a metà (senza altro then) e chiamare poi la seconda metà (secondo then) nel main per completarla
